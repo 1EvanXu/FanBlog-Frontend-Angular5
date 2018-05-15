a9f97a29fc6e8731a592cb67d1fbb2cb77809fe1
Yifan Xu <yifan.xu@citrix.com>
2018-05-15 15:57:52 +0800

    base blog frame complete

---
+++ src/app/nodes/article/article.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-article',
+  template: `
+    <p>
+      article works!
+    </p>
+  `,
+  styles: []
+})
+export class ArticleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
