907043636b1974cbbdb89036d3f209c2c486bd75
yifan.xu <yifan_xu6@163.com>
2018-06-07 20:46:20 +0800

    modify the ErrorComponent

--- src/app/nodes/error/error.component.ts
+++ src/app/nodes/error/error.component.ts
@@ -2,11 +2,65 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   template: `
-    <p>
-      error happens!
-    </p>
+    <div class="error-page-header">
+          <img class="logo" src="../assets/logo.png">
+    </div>
+    <div class="error-container">
+      <table cellspacing="0" cellpadding="0">
+        <tr>
+          <td>
+            <table cellspacing="0" cellpadding="0">
+              <tr>
+                <td>
+                  <h1>404</h1>
+                  <h3>Page Not Found!</h3>
+                  <p>
+                    <a href="/">Back to Home ></a>
+                  </p>
+                </td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+      </table>
+    </div>
   `,
-  styles: []
+  styles: [`
+    *{
+      font-family:"Microsoft Yahei";
+      margin:0;font-weight:lighter;
+      text-decoration:none;
+      text-align:center;
+      line-height:2.2em;
+    }
+    h1 {
+      font-size:100px;
+      line-height:1em;
+      color: #2D2D2D;
+    }
+    h3 {
+      color: #2c2c2c;
+    }
+    table{
+      width:100%;
+      height:100%;
+      border:0;
+    }
+    .error-container{
+      margin-top: 17% ;
+    }
+    .logo {
+      width: 130px;
+      height: 40px;
+      margin: 16px 30px 16px 50px;
+      float: left;
+    }
+    .error-page-header {
+      overflow: auto;
+      display: block;
+      border-bottom: 1px lightgray solid;
+    }
+  `]
 })
 export class ErrorComponent implements OnInit {
 
