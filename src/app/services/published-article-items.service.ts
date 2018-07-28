42c770fbe52ed989522c9590a35f6a0b7ba15c89
yifan.xu <yifan_xu6@163.com>
2018-07-28 17:14:21 +0800

    delete some mock data file

--- src/app/services/published-article-items.service.ts
+++ src/app/services/published-article-items.service.ts
@@ -1,9 +1,6 @@
 import { Injectable } from '@angular/core';
-import {ARTICLE_ITEMS, ARTICLE_ITEMS_1, ARTICLE_ITEMS_2, ARTICLE_ITEMS_3} from '../mock-data/article-items';
-import {of} from 'rxjs/observable/of';
 import {Observable} from 'rxjs/Observable';
-import {delay} from 'rxjs/operators';
-import {PublishedArticleItem, PublishedArticleItemCollection} from '../data-model/publised-article-item';
+import {PublishedArticleItemCollection} from '../data-model/publised-article-item';
 import {PublishedArticlesItemsApiClient} from '../apis/published-article-items.api.service';
 
 
--- src/app/services/sidebar.service.ts
+++ src/app/services/sidebar.service.ts
@@ -1,7 +1,4 @@
 import { Injectable } from '@angular/core';
-import {of} from 'rxjs/observable/of';
-import {CATEGORIES, LATEST_ARTICLES, POPULAR_ARTICLES} from '../mock-data/side-info';
-import {delay} from 'rxjs/operators';
 import {SideInformationApiClient} from '../apis/side-information.api.service';
 import {SideInformationCollection} from '../data-model/side-information';
 import {Observable} from 'rxjs/Observable';
@@ -11,16 +8,7 @@ import {Observable} from 'rxjs/Observable';
 export class SidebarService {
 
   constructor(private _apiClient: SideInformationApiClient) {}
-  //
-  // getLatestArticles() {
-  //   return of(LATEST_ARTICLES).pipe(delay(1000));
-  // }
-  // getPopularArticles() {
-  //   return of(POPULAR_ARTICLES).pipe(delay(1000));
-  // }
-  // getCategories() {
-  //   return of(CATEGORIES).pipe(delay(1000));
-  // }
+
   getLatestArticles(): Observable<SideInformationCollection> {
     return this._apiClient.getLatestArticles();
   }
