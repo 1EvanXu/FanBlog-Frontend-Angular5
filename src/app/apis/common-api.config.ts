5914f61ff0aba1143f0df00ffb75218d9f3bb833
yifan.xu <yifan_xu6@163.com>
2018-10-23 20:07:01 +0800

    hide domain and github client id

--- src/app/apis/common-api.config.ts
+++ src/app/apis/common-api.config.ts
@@ -1,8 +1,8 @@
 
-const Domain = 'https://www.521fan.com';
+const Domain = 'https://www.xxxxxx.com';
 
 export const BASE_API_URL = Domain + '/blog-api/';
-export const GithubOAuth2ClientId = 'f915c34a25009eae0d33';
+export const GithubOAuth2ClientId = 'xxxxxxxxxxxxxxxxxxxx';
 export const GithubOAuth2Url = `https://github.com/login/oauth/authorize?client_id=${GithubOAuth2ClientId}&scope=user,public_repo`;
 
 export const HttpRequestOption = {
